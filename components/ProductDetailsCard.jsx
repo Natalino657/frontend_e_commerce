9e698b38b4c5befc2f4ca7fe559091d5f785d628
Natalino Gomes <intgomes657@gmail.com>
2025-12-08 22:07:00 +0000

    start to implement productDetailcard component

---
+++ components/ProductDetailsCard.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Rating from "./Rating";
+import React from "react";
+
+const ProductDetailsCard = () => {
+  return (
+    <View>
+      <Text>ProductDetailsCard</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsCard;
+
+const styles = StyleSheet.create({});
