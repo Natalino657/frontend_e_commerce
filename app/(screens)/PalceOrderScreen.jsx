ba2b33dd7a6e0401438cbd6491cc2a47fb72d4e9
Natalino Gomes <intgomes657@gmail.com>
2026-01-21 22:11:25 +0000

    updates

---
+++ app/(screens)/PalceOrderScreen.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PalceOrderScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PalceOrderScreen</Text>
+    </View>
+  );
+};
+
+export default PalceOrderScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- app/(screens)/PaymentScreen.jsx
+++ app/(screens)/PaymentScreen.jsx
@@ -12,15 +12,15 @@ const PaymentScreen = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
-  const { ShippingAddress } = cart;
+  const { shippingAddress } = cart;
 
   const [PaymentMethod, setPaymentMethod] = useState("PayPal");
 
   useEffect(() => {
-    if (!ShippingAddress || !ShippingAddress.addrese) {
-      router.replace("(sreens)/ShippingScreen");
+    if (!shippingAddress || !shippingAddress.address) {
+      router.replace("(screens)/ShippingScreen");
     }
-  }, [ShippingAddress, router]);
+  }, [shippingAddress, router]);
 
   const submitHandler = () => {
     if (!PaymentMethod) {
@@ -36,9 +36,30 @@ const PaymentScreen = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <FormContainer>
-        <View style={styles.FormGroup}>
-          <Text Style={styles.label}>Select Method</Text>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Select Method:</Text>
+          <View style={styles.radiosGroup}>
+            <TouchableOpacity
+              style={styles.radioButton}
+              onPress={() => setPaymentMethod("PayPal")}
+            >
+              <MaterialIcons
+                name={
+                  PaymentMethod === "PayPal"
+                    ? "radio-button-checked"
+                    : "radio-button-unchecked"
+                }
+                size={24}
+                color={Colors.primary}
+              />
+              <Text style={styles.radioLabel}>Paypal or Credit Card</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+
+        <TouchableOpacity style={styles.button} onPress={submitHandler}>
+          <Text style={styles.buttonText}>Continue</Text>
+        </TouchableOpacity>
       </FormContainer>
     </SafeAreaView>
   );
@@ -46,4 +67,54 @@ const PaymentScreen = () => {
 
 export default PaymentScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: Colors.offWhite,
+    justifyContent: "center",
+    padding: 15,
+  },
+
+  formGroup: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: Colors.textColor,
+    marginBottom: 10,
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    marginBottom: 10,
+    backgroundColor: Colors.white,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colors.lightGray,
+  },
+
+  radioLabel: {
+    fontSize: 16,
+    color: Colors.textColor,
+    marginLeft: 10,
+  },
+
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    width: "100%",
+  },
+
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
