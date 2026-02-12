b666fb70629c4a11d67ae6586f27673c73ec246e
Natalino Gomes <intgomes657@gmail.com>
2026-02-12 00:46:19 +0000

    orderApiSlice implementation

--- constants/Urls.js
+++ constants/Urls.js
@@ -1,4 +1,9 @@
-//export const BASE_URL = "http://172.20.10.13:5300";
-export const BASE_URL = "http://192.168.1.111:5300";
+export const BASE_URL = "http://172.20.10.13:5300";
+//export const BASE_URL = "http://192.168.1.111:5300";
 export const PRODUCT_URL = "/api/products";
 export const USER_URL = "/api/users";
+
+export const ORDERS_URL = "/api/orders";
+
+export const PAYPAL_API_URL = `${BASE_URL}/api/paypal`;
+export const PAYPAL_CONFIG_URL = `${BASE_URL}/api/config/paypal`;
